dfd2ef02ce90dcd1a3435eb05d41b31020d136d7
Louise726 <louisemarc726@yahoo.com>
2024-03-14 19:13:56 +0100

    requêtes API pour une URL écrite en dur et affichage des données sur la page fonctionnels.

--- projet_web/src/App.js
+++ projet_web/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+
+const xappToken = 'eyJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6IiIsInN1YmplY3RfYXBwbGljYXRpb24iOiI3MWQzNzM0Zi0xMDdjLTQwNDctYjIxYi03ZTU0YTBlNzYwYmMiLCJleHAiOjE3MTEwMzk3NzksImlhdCI6MTcxMDQzNDk3OSwiYXVkIjoiNzFkMzczNGYtMTA3Yy00MDQ3LWIyMWItN2U1NGEwZTc2MGJjIiwiaXNzIjoiR3Jhdml0eSIsImp0aSI6IjY1ZjMyYWEzYWJhMjFiMDAwZDM5N2VkNCJ9.7DO8ErU2wLmmU08k3tL4mv_0obfIS0GMS_j9pwduB9M';
+const URL = 'https://api.artsy.net/api/artists/4d8b92b34eb68a1b2c0003f4' //andy warhol
+//const URL = 'https://api.artsy.net/api/artists/leonardo-da-vinci' //léonard de vinci
 
 function App() {
+
+  const [attributs, setAPI] = useState(0)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(URL, {
+        method : 'GET',
+        headers : {
+          'X-Xapp-Token': xappToken
+        },
+      })
+        .then(response => response.json())
+        .then(response => {
+          setAPI(response)
+        })
+    }
+    fetchData();
+  }, []); // on rajoute des brackets vide pour n'appeler l'API qu'une seule fois
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {attributs.name} est né en {attributs.birthday} à {attributs.hometown}
     </div>
-  );
+  )
 }
 
 export default App;
