a34f41ec5471d60d23282731aa77ad894cfdb8c3
Zixin P <118615352+ZixinP@users.noreply.github.com>
2024-03-19 23:08:31 +0100

    Merge main with ZixinP

--- projet_web/src/App.js
+++ projet_web/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 const xappToken = 'eyJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6IiIsInN1YmplY3RfYXBwbGljYXRpb24iOiI3MWQzNzM0Zi0xMDdjLTQwNDctYjIxYi03ZTU0YTBlNzYwYmMiLCJleHAiOjE3MTEwMzk3NzksImlhdCI6MTcxMDQzNDk3OSwiYXVkIjoiNzFkMzczNGYtMTA3Yy00MDQ3LWIyMWItN2U1NGEwZTc2MGJjIiwiaXNzIjoiR3Jhdml0eSIsImp0aSI6IjY1ZjMyYWEzYWJhMjFiMDAwZDM5N2VkNCJ9.7DO8ErU2wLmmU08k3tL4mv_0obfIS0GMS_j9pwduB9M';
@@ -33,4 +33,80 @@ function App() {
   )
 }
 
-export default App;
+
+
+const imageData = [
+  {
+    url: 'url_to_image1',
+    artist: 'Artist 1',
+    date: 'Date 1',
+    type: 'Type 1'
+  },
+  {
+    url: 'url_to_image2',
+    artist: 'Artist 2',
+    date: 'Date 2',
+    type: 'Type 2'
+  },
+  
+];
+
+const Game = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [showAnswer, setShowAnswer] = useState(false);
+  const { url, artist, date, type } = imageData[currentIndex];
+
+  const handleNext = () => {
+    setShowAnswer(false);
+    setCurrentIndex(currentIndex === imageData.length - 1 ? 0 : currentIndex + 1);
+  };
+  
+
+  return (
+   <div className = "backGround"> 
+    <div className="centered"> {}
+      <h1>Guessing Game</h1>
+      <div className = "row">
+        <img src={url} alt="Artwork" />
+      </div>
+      <div className = "row">
+        <label>Artist: </label>
+        <input type="text" disabled={showAnswer} className = "inputCustom" style = {{marginRight : "10px"}}/>
+        <button className="buttonHint" onClick={() => alert(artist)}>Hint</button>
+        {showAnswer && <div className="answer">Answer: {artist}</div>}
+      </div>
+      <div className = "row">
+        <label>Date: </label>
+        <input type="text" disabled={showAnswer} className = "inputCustom" style = {{marginRight : "10px"}}/>
+        <button className="buttonHint" onClick={() => alert(date)}>Hint</button>
+        {showAnswer && <div className="answer">Answer: {date}</div>}
+      </div>
+      <div className = "row">
+        <label>Type: </label>
+        <input type="text" disabled={showAnswer} className = "inputCustom" style = {{marginRight : "10px"}}/>
+        <button className="buttonHint" onClick={() => alert(type)}>Hint</button>
+        {showAnswer && <div className="answer">Answer: {type}</div>}
+      </div>
+      <div className = "row" style ={{marginLeft:"40px"}}>
+        <button className = "button" onClick={() => setShowAnswer(true)} style= {{marginRight : "25px"}}>Show Answer</button>
+        <button className = "button" onClick={handleNext}>Next</button>
+      </div>
+    </div>
+    <div className="ranking-container">
+        <div className="ranking-title">Ranking</div>
+        <div className="ranking-list">
+          {/* create 10 rows */}
+          {[...Array(10)].map((_, index) => (
+            <div className="ranking-item" key={index}>
+              <span>#{index + 1}</span>
+              <span>Score</span>
+            </div>
+          ))}
+        </div>
+    </div>
+  </div>
+  );
+};
+
+export default Game;
+
