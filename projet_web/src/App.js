4ca671f2718833294fe9b2b37a5769f101606025
Zixin P <118615352+ZixinP@users.noreply.github.com>
2024-03-20 11:44:55 +0100

    replace type by title,add box at button

--- projet_web/src/App.js
+++ projet_web/src/App.js
@@ -38,43 +38,46 @@ function App() {
 const imageData = [
   {
     url: 'url_to_image1',
+    title: 'Title 1',
     artist: 'Artist 1',
     date: 'Date 1',
-    type: 'Type 1'
   },
   {
     url: 'url_to_image2',
+    title: 'Title 2',
     artist: 'Artist 2',
     date: 'Date 2',
-    type: 'Type 2'
   },
   {
     url: 'url_to_image3',
+    title: 'Title 3',
     artist: 'Artist 3',
     date: 'Date 3',
-    type: 'Type 3'
   },
   {
     url: 'url_to_image4',
+    title: 'Title 4',
     artist: 'Artist 4',
-    date: 'Date 4',
-    type: 'Type 4'
-  
+    date: 'Date 4', 
   }
   
 ];
 
 const Game = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [nextIndex, setNextIndex] = useState(0);
+  
   const [showAnswer, setShowAnswer] = useState(false); 
   const [inputDisabled, setInputDisabled] = useState(false);
-  const { url, artist, date, type } = imageData[currentIndex];
+  
+  const { url, title, artist, date } = imageData[currentIndex];
+  
   const [userRank, setUserRank] = useState({ rank: 111, score: 11 });
-  const [nextIndex, setNextIndex] = useState(0);
+  const [scoreRound, setScoreRound] = useState(0);
   const [userAnswers, setUserAnswers] = useState({
+    title: '',
     artist: '',
     date: '',
-    type: ''
   });
 
 
@@ -84,7 +87,7 @@ const Game = () => {
     const nextIndex=currentIndex === imageData.length - 1 ? 0 : currentIndex + 1;
     setNextIndex(nextIndex);
     setTimeout(() => setCurrentIndex(nextIndex), 300);  // 300ms after the nextIndex is set, the currentIndex is updated,in order to show the transition effect
-    setUserAnswers({ artist: '', date: '', type: '' }); 
+    setUserAnswers({ title: '',artist: '', date: ''}); 
     setInputDisabled(false);
   };
 
@@ -98,8 +101,9 @@ const Game = () => {
     const scoreGet = 
       (userAnswers.artist.toLowerCase() === artist.toLowerCase() ? 1 : 0 )+
       (userAnswers.date.toLowerCase() === date.toLowerCase() ? 1 : 0) +
-      (userAnswers.type.toLowerCase() === type.toLowerCase() ? 1 : 0);
+      (userAnswers.title.toLowerCase() === title.toLowerCase() ? 1 : 0);
     
+    setScoreRound(scoreGet);  
     setUserRank(prevState =>({
       ...prevState,
       score: prevState.score + scoreGet
@@ -115,13 +119,25 @@ const Game = () => {
         <img src={url} alt="Artwork" height="400" className={nextIndex !== currentIndex ? "fade-out" : ""}/>
       </div>
       <div className = "row">
-        <label>Artist: </label>
+        <label>Title: </label>
         <input type="text" 
-               value={userAnswers.artist}
+               value={userAnswers.title}
                disabled={showAnswer || inputDisabled} 
                className = "inputCustom" 
                style = {{marginRight : "10px"}}
-               onChange = {(e) => handleInputChange(e, 'artist')}/>
+               onChange = {(e) => handleInputChange(e, 'title')}/>
+        <button className="buttonHint" onClick={() => alert(title)}>Hint</button>
+        {showAnswer && <div className="answer">Answer: {title}</div>}
+      </div>
+      <div className = "row">
+        <label>Artist: </label>
+        <input type="text" 
+               value= {userAnswers.artist}
+               disabled={showAnswer || inputDisabled} 
+               className = "inputCustom" 
+               style = {{marginRight : "10px"}}
+               onChange= {(e) => handleInputChange(e, 'artist')}
+               />
         <button className="buttonHint" onClick={() => alert(artist)}>Hint</button>
         {showAnswer && <div className="answer">Answer: {artist}</div>}
       </div>
@@ -132,34 +148,31 @@ const Game = () => {
                disabled={showAnswer || inputDisabled} 
                className = "inputCustom" 
                style = {{marginRight : "10px"}}
-               onChange= {(e) => handleInputChange(e, 'date')}
+               onChange = {(e) => handleInputChange(e, 'date')}
                />
         <button className="buttonHint" onClick={() => alert(date)}>Hint</button>
         {showAnswer && <div className="answer">Answer: {date}</div>}
-      </div>
-      <div className = "row">
-        <label>Type: </label>
-        <input type="text" 
-               value= {userAnswers.type}
-               disabled={showAnswer || inputDisabled} 
-               className = "inputCustom" 
-               style = {{marginRight : "10px"}}
-               onChange = {(e) => handleInputChange(e, 'type')}
-               />
-        <button className="buttonHint" onClick={() => alert(type)}>Hint</button>
-        {showAnswer && <div className="answer">Answer: {type}</div>}
       </div>
       <div className = "row" style ={{marginLeft:"40px"}}>
         <button className = "button" onClick={() => setShowAnswer(true)} style= {{marginRight : "10px"}}>Show Answer</button>
         <button className = "button" disabled={inputDisabled} onClick={handleSubmission} style={{marginRight:"10px"}}>Submit</button>
         <button className = "button" onClick={handleNext}>Next</button>
       </div>
+      <div className="row">
+        {inputDisabled && (
+          <div className='custom-box'>
+            <div className='extraInfor'>Score of this round: {scoreRound}</div>
+            <div className='extraInfor'>Information of this image:</div>
+            <div className='extraInfor'></div>
+          </div>
+        )}
+      </div>
     </div>
     <div className="ranking-container">
         <div className="ranking-title">Ranking</div>
         <div className="ranking-list">
           {/* create 10 rows */}
-          {[...Array(10)].map((_, index) => (
+          {[...Array(15)].map((_, index) => (
             <div className="ranking-item" key={index}>
               <span>#{index + 1}</span>
               <span>Score</span>
